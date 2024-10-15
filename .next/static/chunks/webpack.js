0f4cf2a5dbd7b83788df7512e764adf0930ffc71
Emmanuel Villada Suarez <emmanuelvillada310016@correo.itm.edu.co>
2024-10-14 21:45:36 -0500

    creando modal para el aviso cuando se suba la obra

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cff8b7f06ae10194"; }
+/******/ 		__webpack_require__.h = function() { return "0994c0508df84c37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
