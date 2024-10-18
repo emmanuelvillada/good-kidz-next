bb8be61d5d7ee373c4f8afe92804ce9360d28341
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2024-10-18 09:52:28 -0500

    cambiando el nombre del icono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <Head>
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/icono.ico"/>
       </Head>
       <body className="min-h-screen flex flex-col">
         <main className="flex-grow h-full">
