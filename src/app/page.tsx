8a634e683298d7d0a55bc12c35f467a4d79ea44e
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2025-02-08 00:47:49 -0500

    creating contributors component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Events from '@/components/Events';
+import Contributors from '@/components/Contributors';
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
         <About />
         <Events />
+        <Contributors />
       </main>
     </>
   );
