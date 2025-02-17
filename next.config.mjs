e14a1fc81e1fb9810661b38fba6cd2be6205aff3
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2025-02-17 17:55:51 -0500

    cleaning the next config

--- next.config.mjs
+++ next.config.mjs
@@ -20,19 +20,6 @@ const nextConfig = {
       }
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac|rtf|doc|docx)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: 'static/media/[name].[hash].[ext]',
-          publicPath: '/_next/',
-        },
-      },
-    });
-    return config;
-  },
 };
 
 export default withNextVideo(nextConfig);
