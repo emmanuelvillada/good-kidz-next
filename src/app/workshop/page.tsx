d7252ffa9b7621e213f12aa6831fed4330323708
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2025-05-20 23:00:22 -0500

    remove WorkshopPage and associated WorkshopForm component

--- src/app/workshop/page.tsx
+++
@@ -1,8 +0,0 @@
-import WorkshopForm from "@/components/form/WorkshopForm";
-
-export default function WorkshopPage() {
-    return (
-        // Render the WorkshopForm component
-        <WorkshopForm />
-    );
-}
